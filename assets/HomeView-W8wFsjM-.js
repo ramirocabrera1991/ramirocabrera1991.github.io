fd765ec7b0f6fa32c4f8b106ce4b3574649def79
Ramiro Cabrera <ramirocabrera1991@gmail.com>
2024-01-12 15:08:18 -0300

    se corrige estilo de login

---
+++ assets/HomeView-W8wFsjM-.js
@@ -0,0 +1,1 @@
+import{N as e}from"./NavBar-IrUh0H41.js";import{d as a,e as r,f as o}from"./index-GPdkVd8U.js";const _={__name:"HomeView",setup(t){return(c,s)=>(a(),r("div",null,[o(e)]))}};export{_ as default};
